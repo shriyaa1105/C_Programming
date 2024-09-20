3dcbf256d325a941cd30081041aafaa2b8cdf78c
shriyaa1105 <166608147+shriyaa1105@users.noreply.github.com>
2024-09-20 12:39:36 +0530

    Add files via upload

---
+++ AREAS.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,l,b,c,s;
+a=2,l=3,b=1,c=2,s=5;
+printf("\n Area of square is %d",s*s);
+printf("\n parameter of square is %d",4*s);
+printf("\n area of rectangle is %d", l* b);
+printf("\n parameter of rectangle is %d", 2*(l+b));
+printf("\n parameter of traingle is %d",(l*b)/2 );
+printf("\n area of triangle is %d",a+b+c);
+getch();
+}
+
+
+
---
+++ FLOAT.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float z = 1.5;
+int e = 5;
+printf("the float number is %.f", 2*(z*e));
+printf("the number is  %f",(z+e));
+printf("the 2nd one is %.2f",(z*e));
+getch();
+}
+
