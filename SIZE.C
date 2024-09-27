2642368efb11c4447d2de572235bc3b3a0ab1700
shriyaa1105 <166608147+shriyaa1105@users.noreply.github.com>
2024-09-27 12:02:20 +0530

    Add files via upload

---
+++ SIZE.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+printf("the size of int is %u bytes",sizeof(int));
+printf("the size of float is %u bytes is ",sizeof(float));
+printf("the size of double is %u bytes is",sizeof(double));
+printf("the size of char is %u byte is",sizeof(char));
+return 0;
+}
+
+
