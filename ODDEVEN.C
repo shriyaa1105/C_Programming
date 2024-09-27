8d65f106023ecf1b7a8b9762a9d8a5f3df009af5
shriyaa1105 <166608147+shriyaa1105@users.noreply.github.com>
2024-09-27 12:13:50 +0530

    Add files via upload

---
+++ ODDEVEN.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+printf("enter any integer:");
+scanf("%d",&a);
+if (a%2==0)
+{
+printf("number is even",a);
+}
+else
+printf("odd",a);
+  return 0;
+  }
