d1950be1e6fbf6f56832632d8d0482dae0c97bd0
shriyaa1105 <166608147+shriyaa1105@users.noreply.github.com>
2024-10-04 12:14:18 +0530

    Update ifelse.c

---
+++ shriya.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int week;
+    printf("Enter any week number");
+    scanf("%d",&week);
+
+    if(week==1){
+        printf("monday");
+    }
+    else if(week==2){
+        printf("tuesday");
+    }
+    else if(week==3){
+        printf("wednesday");
+    }
+    else if(week==4){
+            printf("thrus");
+
+        }
+    else if(week==5){
+            printf("friday");}
+    else {
+                printf("sat");
+            }
+            return 0;
+        }
